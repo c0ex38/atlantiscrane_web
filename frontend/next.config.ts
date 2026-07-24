319fba2e73ac071a3326a1191f25a896605b4b6b
Çağrı Özay <cagriozay13@gmail.com>
2026-07-24 10:11:57 +0300

    chore: remove dev origins from next.config.ts for production readiness

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,21 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  allowedDevOrigins: ["192.168.1.190"],
   images: {
     qualities: [75, 100],
     remotePatterns: [
       {
         protocol: "https",
         hostname: "images.unsplash.com",
-      },
-      {
-        protocol: "http",
-        hostname: "192.168.1.190",
-      },
-      {
-        protocol: "https",
-        hostname: "192.168.1.190",
       },
       {
         protocol: "https",
